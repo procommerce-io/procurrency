06ee4a4592efedd54dcf15f15b85b8191614058f
RKana <rkanamail@gmail.com>
2019-04-24 19:16:26 -0400

    Code cleanup and Copyright year update

--- src/miner.h
+++ src/miner.h
@@ -1,17 +1,14 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2012 The Bitcoin developers
 // Copyright (c) 2013 The NovaCoin developers
+// Copyright (c) 2017-2019 The ProCurrency developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #ifndef NOVACOIN_MINER_H
 #define NOVACOIN_MINER_H
 
 #include "main.h"
-#ifndef OTP_ENABLED
-    #include "wallet.h"
-#else
-    #include "wallet_otp.h"
-#endif
+#include "wallet.h"
 #include "init.h"
 
 void ThreadStakeMiner(CWallet *pwallet);
