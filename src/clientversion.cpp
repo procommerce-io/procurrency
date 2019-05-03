fc9aa6fbc1140fefa9984849b3a32330e796d949
RKana <rkanamail@gmail.com>
2019-05-03 04:46:30 -0400

    proc v1.39.2.2
    v1.39.2.2 LR3
    This update is a "Quick" Fix for v1.39.2.1 - Please check the release
    notes for more
    details
    
    It is RECOMMENDED to all Wallet users and node owners running Older
    versions to update to
    this latest release.
    The Forks have been brought down from block# 629000 instead of 649000
    
    -Set all Forks from block# 629000 and plus
    -FIX: Moved Coinbase Maturity Fork to MerkleTx::GetBlocksToMaturity
    -FIX: Moved Min Stake Age Fork to CTransaction::GetCoinAge
    -FIX: Moved Min TxFee Fork to CTransaction::GetMinFee

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -37,7 +37,7 @@ const std::string CLIENT_NAME("ProcCore");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID ": LR2"
+#    define GIT_COMMIT_ID ": LR3"
 //#    define GIT_COMMIT_DATE "$Format:%cD"
 #endif
 
